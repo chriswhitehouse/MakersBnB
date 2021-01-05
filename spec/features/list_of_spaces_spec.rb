d7197e06bc50efb80ee22eff370e8487858fda3b
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-01-05 16:40:15 +0000

    List of Spaces: Failing Feature Test

---
+++ spec/features/list_of_spaces_spec.rb
@@ -0,0 +1,26 @@
+feature "So i can choose a space" do
+  scenario "I would like to see a list of spaces" do
+    Space.create(
+      name: "Test name",
+      description: "Test description",
+      date_available_from: "2021-01-01",
+      date_available_to: "2021-01-02",
+      price: "50"
+      )
+
+    Space.create(
+      name: "Another name",
+      description: "Another description",
+      date_available_from: "2021-01-11",
+      date_available_to: "2021-02-12",
+      price: "50")
+
+    visit('/spaces')
+    expect(page).to have_selector("submit", :class => "list_a_space")
+    expect(page).not_to have_content('sinatra')
+    expect(page).to have_content('Book a space')
+    expect(page).to have_content("Test name")
+    expect(page).to have_content("Another description")
+    expect(page).to have_content("Another name")
+  end
+end
