69b78d06d1e9ce5704b5718158d60e654e0b6614
PiperS52 <simon.a.piper87@gmail.com>
2021-01-06 15:43:07 +0000

    feature for make request

---
+++ spec/features/make_request_spec.rb
@@ -0,0 +1,24 @@
+feature 'As a user So that i can rent an owners space I would like to be able to request an owners space for one night' do
+  scenario 'user makes a single request' do
+    user = User.create(email: 'test@example.com', password: 'test')
+
+    space = Space.create(
+      name: "Lovely space",
+      description: "Lovely description",
+      date_available_from: "2021-01-01",
+      date_available_to: "2021-01-02",
+      price: "50",
+      user_id: user.id
+      )
+
+    sign_up
+    visit('/spaces')
+    first('.space').click_button 'More details'
+
+    expect(current_path).to eq "/spaces/#{space.id}"
+    expect(page).to have_content("Lovely space")
+    click_button('Make request')
+    expect(current_path).to eq "/spaces"
+
+  end
+end
