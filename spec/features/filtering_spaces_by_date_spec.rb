18e75a7df5ffd54765c83b62d3cbfe72f837143a
ZeenLamDev <Zeen.Lam93@gmail.com>
2021-01-06 10:42:57 +0000

    added feature test for filtering_spaces_available

---
+++ spec/features/filtering_spaces_by_date_spec.rb
@@ -0,0 +1,33 @@
+feature 'So i can see spaces in a specified date range' do
+  scenario 'I would like to be able to filter the list by date of availibility' do
+
+    Space.create(
+      name: "Test name",
+      description: "Test description",
+      date_available_from: "2021-02-02",
+      date_available_to: "2021-02-20",
+      price: "50"
+      )
+
+      Space.create(
+        name: "Another name",
+        description: "Another Test description",
+        date_available_from: "2021-03-09",
+        date_available_to: "2021-03-29",
+        price: "50"
+        )
+
+      visit('/spaces')
+      expect(page).to have_content('Available from')
+      expect(page).to have_content('Available to')
+      expect(page).to have_selector("input", :class => "show_listings")
+      fill_in('available_from',with: '2021-02-01')
+      fill_in('available_to',with: '2021-02-28')
+      click_button('Show listings')
+      expect(page).to have_content('Test name')
+      expect(page).not_to have_content('Another name')
+
+
+    
+  end
+end
