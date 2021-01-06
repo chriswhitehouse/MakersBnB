138e691ef6cd09490750d43a9754376f8a8660ac
PiperS52 <simon.a.piper87@gmail.com>
2021-01-06 22:31:41 +0000

    sign in, sign out and authentication working - relevant rspec passing

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,14 @@
+feature "As a user, so I can make changes to my bookings/listings, I would like to log in" do
+  scenario "A previous user can sign in to the site" do
+    User.create(email: 'test@example.com', password: 'test')
+
+    visit('/')
+    click_button('Sign in')
+    expect(current_path).to eq '/sessions/new'
+
+    fill_in('email', with: 'test@example.com')
+    fill_in('password', with: 'fakepassword123')
+    click_button('Confirm')
+    # expect(current_path).to eq '/sessions'
+  end
+end
